2bedae615b0a0ca8e3c64479eab384bdc4cd5ae2
NSN-INTRA\krystek <blazej.krystek@nokia.com>
2018-03-06 18:50:48 +0100

    add lib test

---
+++ libTest/test/converter-test.js
@@ -0,0 +1,34 @@
+var expect    = require("chai").expect;
+
+
+
+var converter = require("../app/converter");
+
+
+
+
+describe("Color Code Converter", function() {
+
+
+  describe("RGB to Hex conversion", function() {
+    it("converts the basic colors", function() {
+      var redHex   = converter.rgbToHex(255, 0, 0);
+
+      expect(redHex).to.equal("ff0000");
+    });
+  });
+
+
+
+
+
+  describe("Hex to RGB conversion", function() {
+    it("converts the basic colors", function() {
+      var red   = converter.hexToRgb("ff0000");
+
+      expect(red).to.deep.equal([255, 0, 0]);
+    });
+  });
+
+
+});
---
+++ libTest/test/lodash-test.js
@@ -0,0 +1,17 @@
+var _    = require("lodash");
+var expect    = require("chai").expect;
+
+describe("lodash check", function() {
+  describe("lodash check", function() {
+    it("lodash check", function() {
+
+      
+      var currentDate = _.now();
+
+
+      expect(currentDate).to.not.equal(undefined);
+    });
+  });
+
+
+});
