7080af6850cc524471ee075648afd209e9763a06
blkr <ghut668@gmail.com>
2018-03-08 15:23:44 +0100

    handling mongo db + show user details

--- modules/express/adminPanel/app.js
+++ modules/express/adminPanel/app.js
@@ -1,19 +1,35 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express    = require("express"),
+    app        = express(),
+    bodyParser = require("body-parser"),
+    mongoose   = require('mongoose');;
 
-var users =[
-	{name:"Yosemite", userName: "unegunda"}, 
-	{name:"Zions Park", userName:"dudas666"}, 
-	{name:"Yellowstone", userName:"ulcia prymulcia"}
-];
+const options = {
+  useMongoClient: true
+};
 
 //app.use(express.static('public'));
+mongoose.connect("mongodb://localhost/site_prototype", options);
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 
+//SCHEMA SETUP
+var userSchema = new mongoose.Schema({
+    displayName:String,
+    userName: String
+});
+
+var Users = mongoose.model("Users", userSchema);
+
 app.get("/", function(req, res){
-    res.render("users", {users:users});
+    Users.find({}, function(err, userList){
+        if(err){
+            console.log("error");
+        }else{
+            console.log(userList);
+            res.render("index.ejs", {users:userList});
+        }
+    });
+    
 });
 
 app.get("/login", function(req, res){
@@ -21,15 +37,53 @@ app.get("/login", function(req, res){
 });
 
 app.get("/users", function(req, res){
-    res.render("users", {users:users});
+    Users.find({}, function(err, userList){
+        if(err){
+            console.log("error");
+        }else{
+            res.render("users", {users:userList});
+        }
+    });
 });
 
+app.get("/users/new", function(req, res){
+  res.render("add-user");
+}); 
+
+app.get("/users/:userId", function(req, res){
+    var userId = req.params.userId;
+    console.log(userId);
+    Users.findById(userId, function(err, specifiedUser){
+        if(err){
+            console.log(err);
+        }else{
+          console.log(specifiedUser);
+            res.render("edit-user", {user:specifiedUser});
+        }
+    });
+});   
+
+   
+
 app.post("/adduser", function(req, res){
-  users.push( 
-    {
-      name: req.body.name, 
+
+  var newUser = {
+      displayName: req.body.displayName, 
       userName: req.body.userName
-  });
+  };
+
+  Users.create(newUser, function(err, addedUser){
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("users");
+        }
+    });
+});
+
+app.delete("/removeuser", function(req, res){
+  console.log("removeuser");
+  console.log(req.body);
   res.redirect("users");
 });
 
